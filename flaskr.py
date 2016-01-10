6df3c992d630bc8c2da6fc5383c4d1935885823d
Zach Russell <zach@protechig.com>
2016-01-10 02:15:24 -0500

    create view functions

--- flaskr.py
+++ flaskr.py
@@ -28,3 +28,41 @@ def init_db():
 
 if __name__ == '__main__':
     app.run()
+
+@app.route('/')
+def show_entries():
+    cur = g.db.execute(' select titles, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return renter_template('show_entries.html', entries=entries)
+
+app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('instert into entries (title, text) values (?, ?)', [request.form['title'], reqiest.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted!')
+    return redirect(url_for('slow_entries'))
+
+app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST'
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redicet(url_for('show_entries'))
+
+
+
