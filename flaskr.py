627b19bc361e0f7cbc26c972cba1a8bba08d619b
Zach Russell <zach@protechig.com>
2016-01-10 02:54:06 -0500

    debugging

--- flaskr.py
+++ flaskr.py
@@ -1,6 +1,18 @@
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-from contextlib import closing
+# -*- coding: utf-8 -*-
+"""
+    Flaskr
+    ~~~~~~
+
+    A microblog example application written as Flask tutorial with
+    Flask and sqlite3.
+
+    :copyright: (c) 2014 by Armin Ronacher.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash, _app_ctx_stack
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
@@ -9,45 +21,66 @@ SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
 
-# create our little applications :)
+# create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
 
 def init_db():
-    with closing(connect_db()) as db:
+    """Creates the database tables."""
+    with app.app_context():
+        db = get_db()
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
 
-if __name__ == '__main__':
-    app.run()
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    top = _app_ctx_stack.top
+    if not hasattr(top, 'sqlite_db'):
+        sqlite_db = sqlite3.connect(app.config['DATABASE'])
+        sqlite_db.row_factory = sqlite3.Row
+        top.sqlite_db = sqlite_db
+
+    return top.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db_connection(exception):
+    """Closes the database again at the end of the request."""
+    top = _app_ctx_stack.top
+    if hasattr(top, 'sqlite_db'):
+        top.sqlite_db.close()
+
 
 @app.route('/')
 def show_entries():
-    cur = g.db.execute(' select titles, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return renter_template('show_entries.html', entries=entries)
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
 
-app.route('/add', methods=['POST'])
+
+@app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('instert into entries (title, text) values (?, ?)', [request.form['title'], reqiest.form['text']])
-    g.db.commit()
-    flash('New entry was successfully posted!')
-    return redirect(url_for('slow_entries'))
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
 
-app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.method == 'POST'
+    if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
             error = 'Invalid username'
         elif request.form['password'] != app.config['PASSWORD']:
@@ -58,11 +91,14 @@ def login():
             return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
 
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return redicet(url_for('show_entries'))
+    return redirect(url_for('show_entries'))
 
 
-
+if __name__ == '__main__':
+    init_db()
+    app.run()
